1902e416ed42337d0b1e92a913ca43e3686adb03
netshiombozwothe <Zwothe118@gmail.com>
2025-03-27 01:04:58 +0200

    Commit initial files

---
+++ index.js
@@ -0,0 +1,40 @@
+
+let count1 = 0
+let count2 = 0
+let scoreBoard1 =document.getElementById("displayOne")
+let scoreBoard2 = document.getElementById("displayTwo")
+
+function  addOne(){
+   count1 +=1
+   scoreBoard1.textContent = count1
+}
+function addTwo(){
+count1 +=2
+scoreBoard1.textContent = count1
+}
+function addThree(){
+count1 +=3
+scoreBoard1.textContent = count1
+}
+
+function add1(){
+count2 += 1
+scoreBoard2.textContent=count2
+}
+
+function add2(){
+    count2 +=2
+    scoreBoard2.textContent=count2
+}
+
+function add3(){
+    count2 += 3
+    scoreBoard2.textContent=count2
+}
+function resetValues(){
+    count1 = 0
+    count2 = 0
+    scoreBoard1.textContent = 0
+    scoreBoard2.textContent = 0
+    
+}
